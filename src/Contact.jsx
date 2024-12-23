a9a491fd8e884a3c8f771e5431f6c340fed2027b
Mayur A Panchal <112242386+Mayurrr29@users.noreply.github.com>
2024-12-23 21:15:16 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -20,7 +20,7 @@ function Contact(){
         e.preventDefault();
 
         try {
-          const response = await fetch("http://localhost:5000/api/send-email", {
+          const response = await fetch("/api/send-email", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
