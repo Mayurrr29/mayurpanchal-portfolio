24f4d547c226cb04a740573c5fe9e6d4aa386216
Mayur <mp9099644464@gmail.com>
2024-12-23 18:29:45 +0530

    Fix head1 import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "./Body1.css"; 
 import Body1 from "./Body1";
-import Head1 from "./head1";
+import Head1 from "./Head1";    
 function App() {
 return  (<>
 <Head1></Head1>
